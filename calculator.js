b940c5731de806fca5f6a92a9574603ec6e1dada
liam woodward <liamwoodward20@outlook.com>
2025-04-26 12:42:26 +0100

    adding to javascript

--- calculator.js
+++ calculator.js
@@ -1,6 +1,17 @@
-const numberKey = document.getElementById('number-key');
+const addBtn = document.getElementById('add-btn');
+const subtractBtn = document.getElementById('subtract-btn');
+const displayOutput = document.getElementById('display-output');
 
-function addNumbers(){
-  
+
+function addNumbers() {
+  const numberBtn = parseInt(document.getElementById('number-btn'));
 }
 
+addBtn.addEventListener('click', addNumbers());
+
+
+
+
+// const num2 = 5;
+// const divide = num1 / num2;
+// console.log(divide);
