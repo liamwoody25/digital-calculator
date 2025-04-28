b3c12ae68e92da4f7d18989f3b09c1a432baa117
liam woodward <liamwoodward20@outlook.com>
2025-04-28 15:20:14 +0100

    adding to html css and js

--- calculator.js
+++ calculator.js
@@ -1,24 +1,30 @@
-const addBtn = document.getElementById('add-btn');
-const subtractBtn = document.getElementById('subtract-btn');
-const result = document.getElementById('result')
+const addBtn_div = document.getElementById('add-btn');
+const subtractBtn_div = document.getElementById('subtract-btn');
+const numBtn_div = document.getElementById('number-keys')
 const displayinput = document.getElementById('display-output');
 
-function currentDisplay(){
-  const display = '';
-  document.getElementById('display').value;
+function getNumber() {
+  let numInput = parseInt(document.getElementById('num-btn'));
+  
 }
 
 
 
 function addNumber(){
+  let num1 = parseInt(document.getElementById('number-1'));
+  let num2 = parseInt(document.getElementById('number-2'));
+  let result = num1 + num2;
+  document.
+  console.log(num1+num2);
 }
 
 
-addBtn.addEventListener('click', function(){
+
+
+addBtn_div.addEventListener('click', function(){
   addNumber()
 })
 
-
-// const num2 = 5;
-// const divide = num1 / num2;
-// console.log(divide);
+numBtn_div.addEventListener('click', function(){
+  getNumber()
+});
